84f1dbf3b1cc0ea18768cf80dd58710b89d519e7
Jean-Pierre Gygax <gygax@practicomp.ch>
2015-01-03 16:33:15 +0100

    Test image: grid

--- testimage/include/gpc/gui/canvas/test_image_gen.hpp
+++ testimage/include/gpc/gui/canvas/test_image_gen.hpp
@@ -22,19 +22,37 @@ namespace gpc {
             class TestImageGenerator {
             public:
 
-                static const int WIDTH = 1280, HEIGHT = 1024;
+                static const int WIDTH = 1200, HEIGHT = 675;
 
                 auto generate(Canvas *canvas) -> std::vector<RGBA32>
                 {
                     std::vector<RGBA32> img;
 
-                    Canvas::native_color_t clr = canvas->rgba_to_native( {1, 1, 1, 0} );
-                    canvas->clear(clr);
+                    canvas->clear(canvas->rgba_to_native({ 0.8f, 0.8f, 0.8f, 0 }));
+
+                    draw_grid(canvas);
 
                     return img;
                 }
 
             private:
+
+                void draw_grid(Canvas *canvas)
+                {
+                    Canvas::native_color_t red  = canvas->rgba_to_native({1, 0, 0, 1});
+                    Canvas::native_color_t blue = canvas->rgba_to_native({0, 0, 1, 1});
+
+                    // Horizontal
+                    for (int y = 0; y <= HEIGHT; y += 50) {
+                        canvas->fill_rect(0, y-2, WIDTH, 2, red);
+                        canvas->fill_rect(0, y, WIDTH, 2, blue);
+                    }
+                    // Vertical
+                    for (int x = 0; x <= WIDTH; x += 50) {
+                        canvas->fill_rect(x-2, 0, 2, HEIGHT, red);
+                        canvas->fill_rect(x, 0, 2, HEIGHT, blue);
+                    }
+                }
             };
 
         } // ns canvas
