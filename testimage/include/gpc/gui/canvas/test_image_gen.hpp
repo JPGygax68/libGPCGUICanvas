3690c6c71275e7bf95d89b36e017168161b9b629
Jean-Pierre Gygax <gygax@practicomp.ch>
2015-01-03 18:57:07 +0100

    More image tests

--- testimage/include/gpc/gui/canvas/test_image_gen.hpp
+++ testimage/include/gpc/gui/canvas/test_image_gen.hpp
@@ -29,6 +29,8 @@ namespace gpc {
                     canvas = canvas_;
 
                     register_fonts();
+
+                    register_test_image();
                 }
 
                 void register_fonts()
@@ -48,6 +50,10 @@ namespace gpc {
                     // TODO: dispose of fonts
                 }
 
+                void register_image()
+                {
+                }
+
                 auto generate() -> std::vector<RGBA32>
                 {
                     std::vector<RGBA32> img;
@@ -56,11 +62,38 @@ namespace gpc {
 
                     draw_grid();
                     draw_plain_rects();
+                    draw_images(200, 50);
 
                     return img;
                 }
 
             private:
+
+                static auto
+                makeColorInterpolatedRectangle(size_t width, size_t height, const std::array<RGBAFloat, 4> &corner_colors) -> std::vector<RGBA32>
+                {
+                    std::vector<RGBA32> image(width * height);
+
+                    auto it = image.begin();
+                    for (auto y = 0U; y < height; y++) {
+                        for (auto x = 0U; x < width; x++) {
+                            RGBAFloat top = interpolate(corner_colors[0], corner_colors[1], float(x) / float(width));
+                            RGBAFloat bottom = interpolate(corner_colors[2], corner_colors[3], float(x) / float(width));
+                            RGBAFloat color = interpolate(top, bottom, float(y) / float(height));
+                            *it = fromFloat(color);
+                            //*it = { {255, 0, 0, 255} };
+                            it++;
+                        }
+                    }
+
+                    return image;
+                }
+
+                void register_test_image()
+                {
+                    auto pixels = makeColorInterpolatedRectangle(50, 50, { { { 1, 0, 0, 1 }, { 0, 1, 0, 1 }, { 0, 0, 1, 1 }, { 1, 1, 1, 1 } } });
+                    test_image = canvas->register_rgba_image(50, 50, &pixels[0]);
+                }
 
                 void draw_grid()
                 {
@@ -87,9 +120,9 @@ namespace gpc {
 
                 void draw_plain_rects()
                 {
-                    Canvas::native_color_t red = canvas->rgba_to_native({ 1, 0, 0, 1 });
+                    Canvas::native_color_t red   = canvas->rgba_to_native({ 1, 0, 0, 1 });
                     Canvas::native_color_t green = canvas->rgba_to_native({ 0, 1, 0, 1 });
-                    Canvas::native_color_t blue = canvas->rgba_to_native({ 0, 0, 1, 1 });
+                    Canvas::native_color_t blue  = canvas->rgba_to_native({ 0, 0, 1, 1 });
                     Canvas::native_color_t white = canvas->rgba_to_native({ 1, 1, 1, 1 });
 
                     canvas->fill_rect(50, 50, 50, 50, red);
@@ -100,6 +133,24 @@ namespace gpc {
 
                 Canvas *canvas;
                 typename Canvas::font_handle_t font;
+                typename Canvas::image_handle_t test_image;
+
+                void draw_images(int x, int y)
+                {
+                    static const int SEP = 25;
+
+                    canvas->draw_image(x, y, 50, 50, test_image); x += 50;
+                    x += SEP;
+                    canvas->draw_image(x, y, 75, 75, test_image); x += 75;
+                    x += SEP;
+                    canvas->set_clipping_rect(x+5, y+5, 40, 40);
+                    canvas->draw_image(x, y, 50, 50, test_image); x += 50;
+                    canvas->cancel_clipping();
+                    x += SEP;
+                    canvas->draw_image(x, y, 50, 50, test_image, 10, 10); x += 50;
+                    x += SEP;
+                    canvas->draw_image(x, y, 50, 50, test_image, 20, 20); x += 50;
+                }
             };
 
         } // ns canvas
